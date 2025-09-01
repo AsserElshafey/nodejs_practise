2cdbd438490cf361e7b061b9481954066a6bae2a
AsserElshafey <elshafeyasser@yahoo.com>
2025-09-01 23:49:22 +0300

    feat: add error handling and not found middleware, enhance server configuration with security and logging

--- server.ts
+++ server.ts
@@ -1,12 +1,43 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import compression from "compression";
+import { rateLimit } from "express-rate-limit";
 import { generateProducts } from "./utils/fakeData";
 import ProductService from "./services/ProductService";
 import ProductController from "./controllers/productController";
 import ProductsViewController from "./controllers/productsViewController";
 import path from "path";
 import productsRouter from "./routes/productsRoute";
+import ErrorMiddleware from "./middlewares/Error";
+import NotFoundMiddleware from "./middlewares/NotFound";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    xFrameOptions: {
+      action: "deny",
+    },
+  })
+);
+
+app.use(compression());
+
+app.use(morgan("dev"));
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
 
 const PORT: number = 8000;
 
@@ -41,10 +72,10 @@ app.get("/products/:id", (req, res) =>
 // endpoints (Products)
 app.use("/api/products", productsRouter);
 
+// error handler
+app.use(NotFoundMiddleware.handle);
+app.use(ErrorMiddleware.handle);
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/`);
-});
-
-app.get("/not", (req, res) => {
-  res.status(404).render("notFound", { title: "Not Found!" });
 });
